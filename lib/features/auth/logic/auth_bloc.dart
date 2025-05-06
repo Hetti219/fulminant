ac9d01dedf344056e1f74ebdd7b5f2f9774740f8
Hetti219 <sathikahettiarachchi219@gmail.com>
2025-05-06 10:50:28 +0530

    Update Authentication BLoC

--- lib/features/auth/logic/auth_bloc.dart
+++ lib/features/auth/logic/auth_bloc.dart
@@ -1,13 +1,42 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import '../data/auth_repo.dart';
 
 part 'auth_event.dart';
+
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(AuthInitial()) {
-    on<AuthEvent>((event, emit) {
-      // TODO: implement event handler
+  final AuthRepository _authRepo;
+
+  AuthBloc(this._authRepo) : super(AuthInitial()) {
+    on<AuthSignUpRequested>((event, emit) async {
+      emit(AuthLoading());
+      try {
+        await _authRepo.signUp(email: event.email, password: event.password);
+        emit(AuthSuccess());
+      } catch (e) {
+        emit(AuthFailure(e.toString()));
+      }
+    });
+
+    on<AuthLoginRequested>((event, emit) async {
+      emit(AuthLoading());
+      try {
+        await _authRepo.logIn(email: event.email, password: event.password);
+        emit(AuthSuccess());
+      } catch (e) {
+        emit(AuthFailure(e.toString()));
+      }
+    });
+
+    on<AuthLogoutRequested>((event, emit) async {
+      emit(AuthLoading());
+      try {
+        await _authRepo.logOut();
+        emit(AuthInitial());
+      } catch (e) {
+        emit(AuthFailure(e.toString()));
+      }
     });
   }
 }
--- lib/features/auth/logic/auth_event.dart
+++ lib/features/auth/logic/auth_event.dart
@@ -4,11 +4,13 @@ abstract class AuthEvent {}
 
 class AuthSignUpRequested extends AuthEvent {
   final String email, password;
+
   AuthSignUpRequested(this.email, this.password);
 }
 
 class AuthLoginRequested extends AuthEvent {
   final String email, password;
+
   AuthLoginRequested(this.email, this.password);
 }
 
