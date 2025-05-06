e4412b42667883e511724c226cc2bec4a6c41d79
Hetti219 <sathikahettiarachchi219@gmail.com>
2025-05-06 11:08:22 +0530

    Add auth_wrapper.dart

---
+++ lib/features/auth/presentation/screens/auth_wrapper.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../../logic/auth_bloc.dart';
+import 'login_screen.dart';
+import '../../../home/home_screen.dart'; // dummy screen for now
+
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          // Show splash/loading screen
+          return const Scaffold(body: Center(child: CircularProgressIndicator()));
+        } else if (snapshot.hasData) {
+          // Logged in
+          return const HomeScreen();
+        } else {
+          // Not logged in
+          return const LoginScreen();
+        }
+      },
+    );
+  }
+}
